c4ede2ec5603e3f863e1c37b014fb864d2b60a5c
abangira <robert.abangira@gmail.com>
2010-02-24 16:13:27 +0200

    added incoming message handler

--- apps/ubuzima/app.py
+++ apps/ubuzima/app.py
@@ -1,6 +1,11 @@
 import rapidsms
 
+from rapidsms.parsers.keyworder import Keyworder
+
 class App (rapidsms.app.App):
+    
+    keyword = Keyworder()
+    
     def start (self):
         """Configure your app in the start phase."""
         pass
@@ -11,8 +16,16 @@ class App (rapidsms.app.App):
 
     def handle (self, message):
         """Add your main application logic in the handle phase."""
-        pass
-
+        results = self.keyword.match(self, message.text)
+        
+        if results:
+            func, captures = results
+            return func(self, message, *captures)
+        else:
+            self.debug("NO MATCH FOR %s" % message.text)
+            message.respond("We don't recogniz this messge")
+            return True
+    
     def cleanup (self, message):
         """Perform any clean up after all handlers have run in the
            cleanup phase."""
@@ -25,3 +38,20 @@ class App (rapidsms.app.App):
     def stop (self):
         """Perform global app cleanup when the application is stopped."""
         pass
+
+
+    @keyword("reg (whatever)")
+    def register(self, message, notice):
+        self.debug("REG message: %s" % message.text)
+        return True
+        
+    @keyword("sup (whatever)")
+    def supervisor(self, message, notice):
+        self.debug("SUP message: %s" % message.text)
+        return True
+    
+    @keyword("pre (whatever)")
+    def pregnancy(self, message, notice):
+        self.debug("PRE message: %s" % message.text)
+        return True
+        
