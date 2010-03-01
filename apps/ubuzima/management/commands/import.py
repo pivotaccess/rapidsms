14b525c4970b8690b184567dd987857b5c8e3615
EricK <e.gasana@pivotaccess.com>
2010-03-01 10:48:02 +0200

    import District and Sector from the new CSVs into the DB

--- apps/ubuzima/management/commands/import.py
+++ apps/ubuzima/management/commands/import.py
@@ -12,10 +12,6 @@ IMPORT_DIR = os.path.abspath("apps/ubuzima/import")
 #the indexes in fosa_table.csv
 PROVINCE_NAME = 5 
 PROVINCE_CODE = 4
-DISTRICT_NAME = 7
-DISTRICT_CODE = 6
-SECTOR_CODE = 8
-SECTOR_NAME = 9
 NAME = 2
 CODE = 0
 TYPE = 3
@@ -29,6 +25,16 @@ VILLAGE_CODE =  0
 VILLAGE_DISTRICT_CODE = 6
 VILLAGE_SECTOR_CODE = 7
 
+#indexes for District.csv
+DISTRICT_NAME = 2
+DISTRICT_CODE = 1
+DISTRICT_PROVINCE_CODE =  3
+
+#indexes for Sector.csv
+SECTOR_CODE = 1
+SECTOR_NAME = 2
+SECTOR_DISTRICT_CODE = 5
+
 class Command(NoArgsCommand):
     
     
@@ -36,7 +42,7 @@ class Command(NoArgsCommand):
         """Returns a CSV reader for _filename_
            relative to the sources directory."""
         path = os.path.join(IMPORT_DIR, filename)
-        return csv.reader(file(path))
+        return csv.reader(open(path, "rU"))
     
     
     def _hospital_name(self, original):
@@ -56,46 +62,59 @@ class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
         #delete all locations before insertion, 
-        #we might change his during production
+        #we might change this during production
         Location.objects.all().delete()
         
         
         rows = list(self._csv("fosa_table.csv"))
+
         rows = rows[1:]
         for row in rows:
             # ensure that the province exists
             province, p_created = \
                 self._loc_type("province").locations.get_or_create(
                     name=row[PROVINCE_NAME].capitalize(),
-                    code=row[PROVINCE_CODE])
+                    code="0" + row[PROVINCE_CODE])
 
             if p_created:
                 print ". Created Province: %s" % (province)
+                self.provinces[province.code] = province
 
+
+        district_rows = list(self._csv("District.csv"))
+        district_rows = district_rows[1:]
+        for row in district_rows:
             # ensure that the district exists, and is
             # linked to the province named on this row
             district, d_created = \
                     self._loc_type("district").locations.get_or_create(
-                        parent=province,
+                        parent = self.provinces[row[DISTRICT_PROVINCE_CODE]],
+                        #parent = province,
                         name=row[DISTRICT_NAME].capitalize(),
-                        code=row[DISTRICT_CODE])
+                        code=row[DISTRICT_CODE]
+                        )
             
             if d_created:
                     print ". Created District: %s" % (district)
                     self.districts[district.code] = district
 
+
+        sector_rows = list(self._csv("Sector.csv"))
+
+        sector_rows = sector_rows[1:] 
+        for row in sector_rows:  
             # making sure the sector exist linked to the district
-            if not row[SECTOR_CODE] in self.sectors:
-                    
-                sector, s_created= \
+            sector, s_created = \
                     self._loc_type("sector").locations.get_or_create(
-                        parent=district,
-                        name=row[SECTOR_NAME].capitalize(),
-                        code=row[SECTOR_CODE]
+                    parent = self.districts[row[SECTOR_DISTRICT_CODE]],
+                    #parent = district,
+                    name=row[SECTOR_NAME].capitalize(),
+                    code=row[SECTOR_CODE]
                     )
-                if s_created:
-                    print ". Created Sector: %s" % (sector)
+            if s_created:
+                    print ". Created Sector: %s (%s)" % (sector, sector.code)
                     self.sectors[sector.code] = sector
+                
         
         # second iteration: create all of the hospitals. we must do
         # this before the health centres, since many health centres
@@ -114,7 +133,8 @@ class Command(NoArgsCommand):
                 print "Adding Hopital: %s %s" % (row[NAME], row[CODE])
                 hospital, created = \
                     self._loc_type("hospital").locations.get_or_create(
-                        parent=self.sectors[row[SECTOR_CODE]],
+                        #parent=self.sectors[row[SECTOR_CODE]],
+                        parent = sector,
                         name=self._hospital_name(row[NAME]),
                         code=fosa_to_code(row[CODE]),
                         latitude=lat,
@@ -180,12 +200,11 @@ class Command(NoArgsCommand):
         
         village_rows = village_rows[1:]
         for row in village_rows:
-            if row[VILLAGE_SECTOR_CODE] in self.sectors:
-                parent = self.sectors[row[VILLAGE_SECTOR_CODE]]
-            elif row[VILLAGE_DISTRICT_CODE] in self.districts:
-                parent = self.districts[row[VILLAGE_DISTRICT_CODE]]
+            sectorCode = "0" + row[VILLAGE_SECTOR_CODE]
+            if sectorCode in self.sectors:
+                parent = self.sectors[sectorCode]
             else:
-                print "Unable to find parent for village: %s" % row[VILLAGE_CODE]
+                print "Unable to find parent for village: %s (sector: %s)" % (row[VILLAGE_CODE], sectorCode)
                 continue
             
             village, v_created= \
